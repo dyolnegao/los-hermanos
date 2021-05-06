992de19a2299df54cb5e221181dd46fa6fca674f
AUGUSTO ROSASCO <52939868+augustorosasco@users.noreply.github.com>
2021-05-05 22:23:18 -0300

    Flask

---
+++ flask_compass/flask_compass/models.py
@@ -0,0 +1,20 @@
+#from flask_login import UserMixin
+#from werkzeug.security import generate_password_hash
+
+from .extensions import db 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    username = db.Column(db.String(100))
+    cidade = db.Column(db.String(50))
+    estado = db.Column(db.String(50))
+    password = db.Column(db.String(100))
+
+"""    @property
+    def unhashed_password(self):
+        raise AttributeError('Cannot view unhashed password!')
+
+    @unhashed_password.setter
+    def unhashed_password(self, unhashed_password):
+        self.password = generate_password_hash(unhashed_password)"""
